03ea452ada4d5f5f7726cc3422e04278783b2c48
cegijon <53779413+cegijon@users.noreply.github.com>
2020-04-03 23:26:12 +1100

    modificando el estado con eventos

--- src/App.js
+++ src/App.js
@@ -4,24 +4,51 @@ import './App.css';
 const App = () => {
 
   const ElementoDeLaLista = ({ tarea, i }) => (
-    <li>
-      {tarea}
-      <button onClick={() => handleClick(i)}>x</button>
+    <li className={tarea.completada ? 'completada' : ''}>
+      {tarea.nombre}
+      <button onClick={() => handleEraseClick(i)}>x</button>
+      <button onClick={() => handleCompleteClick(i)}>Marcar como completada</button>
     </li>
   )
 
   const [tareas, setTareas] = useState([
-    "Conectarse a la clase",
-    "No trabajar durante la clase",
-    "Halagar los fondos creativos de la profesora",
-    "Ganarse el pan de cada dia",
-    "Ganar el bingo",
-    "Salvar el mundo",
+    { nombre: "Conectarse a la clase", completada: true },
+    { nombre: "No trabajar durante la clase", completada: false },
+    { nombre: "Halagar los fondos creativos de la profesora", completada: true },
+    { nombre: "Ganarse el pan de cada dia", completada: true },
+    { nombre: "Ganar el bingo", completada: false },
+    { nombre: "Salvar el mundo", completada: true },
   ])
 
-  const handleClick = indiceTareaABorrar => {
+  const [nuevaTarea, setNuevaTarea] = useState('')
+
+  const handleCompleteClick = indiceTareaACompletar => {
+    const tareasModificadas = tareas.map((tarea, i) => {
+      tarea.completada = i === indiceTareaACompletar ? !tarea.completada : tarea.completada
+      return tarea
+    })
+    setTareas(tareasModificadas)
+  }
+
+  const handleEraseClick = indiceTareaABorrar => {
     const tareasFiltradas = tareas.filter((tarea, i) => i !== indiceTareaABorrar)
     setTareas(tareasFiltradas)
+  }
+
+  const handleChange = e => {
+    setNuevaTarea(e.target.value)
+  }
+
+  const handleSubmit = e => {
+    e.preventDefault()
+
+    const listaDeTareas = tareas
+    const objeto = {
+      nombre: nuevaTarea,
+      completada: false,
+    }
+    listaDeTareas.push(objeto)
+    setNuevaTarea(listaDeTareas)
   }
 
   return (
@@ -29,6 +56,10 @@ const App = () => {
       <ul>
         {tareas.map((tarea, i) => <ElementoDeLaLista key={i} tarea={tarea} i={i} />)}
       </ul>
+      <form onSubmit={handleSubmit}>
+        <input type="text" placeholder="Agregar una tarea..." onChange={handleChange} />
+        <input type="submit" value="Agregar" />
+      </form>
     </div>
   );
 }
