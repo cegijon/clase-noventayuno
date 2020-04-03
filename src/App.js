7e56fc63983716d77b88ad02cb1e8f8f81e0e655
cegijon <53779413+cegijon@users.noreply.github.com>
2020-04-03 21:40:46 +1100

    codigo componentizado

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
+
+  const ElementoDeLaLista = ({ tarea, i }) => (
+    <li>
+      {tarea}
+      <button onClick={() => handleClick(i)}>x</button>
+    </li>
+  )
+
+  const [tareas, setTareas] = useState([
+    "Conectarse a la clase",
+    "No trabajar durante la clase",
+    "Halagar los fondos creativos de la profesora",
+    "Ganarse el pan de cada dia",
+    "Ganar el bingo",
+    "Salvar el mundo",
+  ])
+
+  const handleClick = indiceTareaABorrar => {
+    const tareasFiltradas = tareas.filter((tarea, i) => i !== indiceTareaABorrar)
+    setTareas(tareasFiltradas)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <ul>
+        {tareas.map((tarea, i) => <ElementoDeLaLista key={i} tarea={tarea} i={i} />)}
+      </ul>
     </div>
   );
 }
